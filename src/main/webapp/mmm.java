ab83077d83f473a3da65bbb47641ef7c1fc24f82
BobWu <wubo.u@qq.com>
2020-08-26 17:06:09 +0800

    basic

--- src/main/webapp/mmm.java
+++ src/main/webapp/mmm.java
@@ -30,6 +30,11 @@ public class mmm extends HttpServlet {
         out.println("<h1>oh yeah： " + System.getenv("ACCESSKEY") + "</h1>");
 
 
+        // 处理中文
+        String name =new String(request.getParameter("name").getBytes("ISO-8859-1"),"UTF-8");
+        out.println("<h1>" +name + "</h1>");
+
+
         String driver = "com.mysql.jdbc.Driver";
         String username = System.getenv("ACCESSKEY");
         String password = System.getenv("SECRETKEY");
@@ -42,6 +47,7 @@ public class mmm extends HttpServlet {
             Connection conn = DriverManager.getConnection(dbUrl,username,password);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("show status");
+            //ResultSet rs = stmt.executeQuery("select * from `test`");
             out.println("<h1>正在执行SQL</h1>");
             while(rs.next()){
                 response.getWriter().println(rs.getString("Variable_name") + " : " +rs.getString("value"));
@@ -50,6 +56,7 @@ public class mmm extends HttpServlet {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             out.println("<h1>系统出错A</h1>");
+            out.println(e.toString() );
         } catch (SQLException e) {
             e.printStackTrace();
             out.println("<h1>系统出错B</h1>");
